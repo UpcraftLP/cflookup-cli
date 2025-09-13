f4ada2babd8ec493937dcbda15eb52a47b1553b0
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-09-13 22:36:11 +0200

    ✨ it works

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub mod cflookup;
+mod net;
+
+pub const USER_AGENT: &str = concat!(
+    env!("CARGO_PKG_NAME"),
+    "/",
+    env!("CARGO_PKG_VERSION"),
+    " (",
+    env!("CARGO_PKG_REPOSITORY"),
+    ")"
+);
